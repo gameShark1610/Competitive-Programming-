2340fe8cd0b50af65c3b233d11b5a1238e41af2c
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2026-04-22 17:01:43 -0600

    icpc 1ra fecha

--- Codeforces/Game with Integers.cpp
+++ Codeforces/Game with Integers.cpp
@@ -2,14 +2,16 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         int n;
-        cin>>n;
-        if(n%3==0) cout<<"Second\n";
-        else cout<<"First\n";
+        cin >> n;
+        if (n % 3 == 0) cout << "Second\n";
+        else cout << "First\n";
     }
     return 0;
 }
---
+++ Contests/2026_ICPC_GPM_1ra_Fecha/Erasmus Valthron.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<pair<vector<int>,int>> num(1e6+1);
+
+void obtenerFactores(int n)
+{
+    vector<int> aux;
+    vector<int> check;
+
+    while (n>1)
+    {
+        for
+    }
+
+
+}
++
+void sol(){
+    int n,q; cin>>n>>q;
+
+    vector<int> aux;
+    aux.push_back(1);
+    num[1].first=aux;
+    num[1].second=1;
+    for (int i = 2; i < 1e6; ++i)
+    {
+
+    }
+
+
+}
+
+int main()
+{
+    int t=1;
+    cin>>t;
+    while (t--) sol();
+    return 0;
+}
