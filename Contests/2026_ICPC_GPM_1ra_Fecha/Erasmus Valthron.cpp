13146576403e6b0d01b38ebc7a9bf838f9ba4c78
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2026-04-23 18:13:14 -0600

    contest

--- Contests/2026_ICPC_GPM_1ra_Fecha/Erasmus Valthron.cpp
+++ Contests/2026_ICPC_GPM_1ra_Fecha/Erasmus Valthron.cpp
@@ -2,31 +2,42 @@
 
 using namespace std;
 
-vector<pair<vector<int>,int>> num(1e6+1);
+vector<int> primoPequeno(1e6+1);
+vector<pair<vector<int>,int>> num(1e6+2);
 
-void obtenerFactores(int n)
-{
-    vector<int> aux;
-    vector<int> check;
-
-    while (n>1)
-    {
-        for
-    }
-
-
+void lowestPrimeSieve(int n) {
+    iota(primoPequeno.begin(),primoPequeno.end(),0);
+    for (int i = 4; i <= n; i +=2) primoPequeno[i]=2;
+    for (int i = 3; i*i <= n; i +=2)
+        if (primoPequeno[i] == i)
+            for (int j = i*i; j <= n; j += 2*i)
+                primoPequeno[j] = min(primoPequeno[j],i);
 }
-+
+
+
 void sol(){
     int n,q; cin>>n>>q;
 
-    vector<int> aux;
-    aux.push_back(1);
-    num[1].first=aux;
-    num[1].second=1;
-    for (int i = 2; i < 1e6; ++i)
-    {
+    lowestPrimeSieve(1e6);
 
+    num[1].second=1;
+    //calculo de los primos
+    for (int i = 2; i <= n; ++i)
+    {
+        int numer = i;
+        num[i].second=numer;
+        while (numer>1) {
+            num[i].first.push_back(primoPequeno[numer]);
+            numer/=primoPequeno[numer];
+        }
+    }
+
+
+    sort(num.begin()+1,num.begin()+n+1);
+
+    for (int i = 0; i < q; ++i) {
+        int query; cin>>query;
+        cout<<num[query].second<<"\n";
     }
 
 
@@ -34,8 +45,9 @@ void sol(){
 
 int main()
 {
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);
     int t=1;
-    cin>>t;
+    //cin>>t;
     while (t--) sol();
     return 0;
 }
