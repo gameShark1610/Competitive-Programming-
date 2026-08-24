35e4ae6cb9990271eba055554a79592a939d00b7
Yair Arciniega <yairarva@gmail.com>
2026-08-24 16:01:29 -0600

    Proyecto Euler

---
+++ Project Euler/p0.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main(){
+    long long num=461000/2;
+
+    long long ans= num*(2*num-1)*(2*num+1)/3;
+    cout<<ans;
+
+
+    
+    return 0;
+}
